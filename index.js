d0de484424081cd986caab734f96060380807686
Ilya <ilya925512@gmail.com>
2024-03-08 21:56:00 +0300

    add create acc logics

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const dotenv = require('dotenv');
-const { Bot, GrammyError, HttpError } = require('grammy');
+const { Bot, GrammyError, HttpError, Keyboard } = require('grammy');
 
 dotenv.config();
 
@@ -17,6 +17,9 @@ bot.api.setMyCommands([
     },
     {
         command: 'imagination', description: 'go to space of thoughts'
+    },
+    {
+        command: 'create_account', description: 'management of accounts'
     }
 ])
 
@@ -29,18 +32,58 @@ bot.on('message:voice', async (ctx) => {
 
 
 
+
+// account management
+
+
+
+bot.hears('Enter username', async (ctx) => {
+    ctx.reply('Enter your username using in tour message: "My username: "');
+});
+
+
+bot.hears('Enter password', async (ctx) => {
+    ctx.reply('Enter your email using in tour message: "My email: "');
+});
+
+
+bot.hears('Enter Email', async (ctx) => {
+    ctx.reply('Enter your password using in tour message: "My password: "');
+})
+
+bot.hears(/My username: /, async (ctx) => {
+    const username = ctx.message.text.slice(13);
+    await ctx.reply(username);
+})
+
+bot.hears(/My email: /, async (ctx) => {
+    const email = ctx.message.text.slice(10);
+    ctx.reply(email);
+})
+
 // commands
 
+
+bot.command('create_account', async (ctx) => {
+    const accountManagement = new Keyboard().text('Enter username').row().text('Enter password').row().text('Enter Email').resized();
+    await ctx.reply('Choose an action...', {
+        reply_markup: accountManagement
+    })
+})
+
 bot.command('imagination', async (ctx) => {
-    await ctx.reply('Go into imagination space: <a href="http://localhost:3000"> Imagination </a>', {
+    await ctx.reply('Go into <a href="http://localhost:3000">imagination space</a>', {
         parse_mode: 'HTML'
     });
     await ctx.react('❤‍🔥');
 })
 
+
+
 bot.command(hiCommands, async (ctx) => {
     await ctx.reply('hi!');
 })
+
 // start checking
 
 bot.command('start', async (ctx) => {
@@ -50,13 +93,13 @@ bot.command('start', async (ctx) => {
     await ctx.react('⚡');
 })
 
-bot.on('msg', async (ctx) => {
+/*bot.on('msg', async (ctx) => {
     await ctx.reply(`<b>${ctx.from.first_name}</b>,  You shouldn't just send messages like that!`, {
         reply_parameters: {message_id: ctx.msg.message_id},
         parse_mode: 'HTML'
     });
     await ctx.react('😐');
-})
+})*/
 
 
 
