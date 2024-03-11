d0af88e392bff01ae99527a27b9a11a41335bbb3
Ilya <ilya925512@gmail.com>
2024-03-11 22:03:42 +0300

    add create acc logics with axios

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const dotenv = require('dotenv');
 const { Bot, GrammyError, HttpError, Keyboard } = require('grammy');
+const axios = require('axios');
 
 dotenv.config();
 
@@ -35,37 +36,60 @@ bot.on('message:voice', async (ctx) => {
 
 // account management
 
+bot.hears('Confirm action', async (ctx) => {
+    await ctx.reply('If you want to create account enter your registration data: \n\n Enter your username using in tour message: "username: " \n\n  Enter your email using in tour message: "email: " \n\n Enter your password using in tour message: "password: " ', {
+        reply_parameters: { message_id: ctx.message.message_id}
+    });
+    await ctx.react('👌');
 
-
-bot.hears('Enter username', async (ctx) => {
-    ctx.reply('Enter your username using in tour message: "My username: "');
-});
-
-
-bot.hears('Enter password', async (ctx) => {
-    ctx.reply('Enter your email using in tour message: "My email: "');
-});
-
-
-bot.hears('Enter Email', async (ctx) => {
-    ctx.reply('Enter your password using in tour message: "My password: "');
 })
 
-bot.hears(/My username: /, async (ctx) => {
-    const username = ctx.message.text.slice(13);
-    await ctx.reply(username);
-})
 
-bot.hears(/My email: /, async (ctx) => {
-    const email = ctx.message.text.slice(10);
-    ctx.reply(email);
+
+let username, password, email;
+
+
+bot.hears(/username: /, async (ctx) => {
+    username = ctx.message.text.slice(10);
+    await ctx.reply(`Username <b>${username}</b> successfully entered`, {
+        parse_mode: 'HTML'
+    });
+});
+
+bot.hears(/email: /, async (ctx) => {
+    email = ctx.message.text.slice(7);
+    await ctx.reply(`Email <b>${email}</b> successfully entered`, {
+        parse_mode: 'HTML'
+    });
+});
+
+bot.hears(/password: /, async (ctx) => {
+    password = ctx.message.text.slice(10);
+    await ctx.reply(`Password <b>${password}</b> successfull6y entered`, {
+        parse_mode: 'HTML'
+    });
+});
+
+bot.hears('Send entered reg data', async (ctx) => {
+    if (username && password && email) {
+        const src = 'http://localhost:8080/auth/registration';
+        await axios.post(src, {
+            username, 
+            password,
+            email
+        }).then(res => console.log(res.data));
+        await ctx.reply('Account successfully created');
+        await ctx.react('❤‍🔥');
+    } else if (!(username && password && email)) {
+        await ctx.reply('Enter registration data')
+    }
 })
 
 // commands
 
 
 bot.command('create_account', async (ctx) => {
-    const accountManagement = new Keyboard().text('Enter username').row().text('Enter password').row().text('Enter Email').resized();
+    const accountManagement = new Keyboard().text('Confirm action').row().text('Send entered reg data').row().resized().oneTime();
     await ctx.reply('Choose an action...', {
         reply_markup: accountManagement
     })
